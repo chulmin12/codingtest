290c987dd09f77f017c75f36e855c2256d494f4c
chulmin12 <gogurye97@gmail.com>
2023-11-27 13:27:09 +0900

    2023.11.27

---
+++ 2주차/2468.cpp
@@ -0,0 +1,8 @@
+//
+//  2468.cpp
+//  codingtest
+//
+//  Created by Hwangchulmin on 2023/11/23.
+//
+
+#include <stdio.h>
---
+++ 2주차/2583.cpp
@@ -0,0 +1,8 @@
+//
+//  2583.cpp
+//  codingtest
+//
+//  Created by Hwangchulmin on 2023/11/27.
+//
+
+#include <stdio.h>
